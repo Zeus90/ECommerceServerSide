6ed2d479b7d881a8b8433a74c10808b8de4989be
Zeuska90 <zeus.ka90@gmail.com>
2023-11-09 23:17:28 +0100

    init commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Data;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly EcommerceContext ecommerceContext;
+
+        public ProductsController(EcommerceContext ecommerceContext)
+        {
+            this.ecommerceContext = ecommerceContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            var products = ecommerceContext.Products.ToList();
+
+            if (products != null)
+            {
+                Console.WriteLine("Hello from controller");
+                return products;
+            }
+
+            return NotFound();
+        }
+    }
+}
